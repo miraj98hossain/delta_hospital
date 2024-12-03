84383584f0f8d818eebbd17a1fccafc423ce5114
miraj98hossain <miraj09hossain@gmail.com>
2024-12-03 22:42:57 +0600

    director notice board ui done

--- lib/presentations/management/view/director_notice/director_notice_view.dart
+++ lib/presentations/management/view/director_notice/director_notice_view.dart
@@ -118,17 +118,216 @@ class _DirectorNoticeViewState extends State<DirectorNoticeView>
                 physics: const NeverScrollableScrollPhysics(),
                 controller: _tabController,
                 children: [
-                  Container(
-                    color: appTheme.white,
+                  Column(
+                    children: [
+                      const CommonTextField(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Expanded(
+                        child: ListView.separated(
+                          itemCount: 10,
+                          separatorBuilder: (context, index) {
+                            return const SizedBox(
+                              height: 10,
+                            );
+                          },
+                          itemBuilder: (context, index) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                color: appTheme.white,
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: 40,
+                                    width: 40,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                        color: appTheme.primary,
+                                      ),
+                                    ),
+                                    child: Center(
+                                      child: Icon(
+                                        Icons.notifications_none_outlined,
+                                        color: appTheme.primary,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Lorem Ipsum is simply dummy",
+                                          style: lightTextTheme.bodySmall!
+                                              .copyWith(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 14,
+                                            color: appTheme.black,
+                                          ),
+                                        ),
+                                        Text(
+                                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the...",
+                                          style: lightTextTheme.bodySmall!
+                                              .copyWith(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 10,
+                                            color: appTheme.black,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
                   ),
-                  Container(
-                    color: appTheme.white,
+                  Column(
+                    children: [
+                      const CommonTextField(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Expanded(
+                        child: ListView.separated(
+                          itemCount: 10,
+                          separatorBuilder: (context, index) {
+                            return const SizedBox(
+                              height: 10,
+                            );
+                          },
+                          itemBuilder: (context, index) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                color: appTheme.white,
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Lorem Ipsum is simply dummy",
+                                    style: lightTextTheme.bodySmall!.copyWith(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 14,
+                                      color: appTheme.black,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the...",
+                                    style: lightTextTheme.bodySmall!.copyWith(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 10,
+                                      color: appTheme.black,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          const Icon(
+                                            Icons.calendar_today_outlined,
+                                            size: 12,
+                                            color: Colors.grey,
+                                          ),
+                                          const SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text(
+                                            "30 Nov 2023",
+                                            style: lightTextTheme.bodySmall!
+                                                .copyWith(
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 10,
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(
+                                        width: 10,
+                                      ),
+                                      Row(
+                                        children: [
+                                          const Icon(
+                                            Icons.person_2_outlined,
+                                            size: 12,
+                                            color: Colors.grey,
+                                          ),
+                                          const SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text(
+                                            "John Doe",
+                                            style: lightTextTheme.bodySmall!
+                                                .copyWith(
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 10,
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
                   ),
                 ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CommonTextField extends StatelessWidget {
+  const CommonTextField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: lightTextTheme.bodySmall!.copyWith(
+        fontSize: 12,
+      ),
+      decoration: InputDecoration(
+        fillColor: appTheme.white,
+        filled: true,
+        contentPadding: const EdgeInsets.all(10),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        prefixIcon: const Icon(
+          Icons.search,
+          color: Colors.grey,
+        ),
+        hintText: "Search",
       ),
     );
   }
