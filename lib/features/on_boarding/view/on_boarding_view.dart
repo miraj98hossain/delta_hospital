980ad1c7b8bc151c3f29947e74051e987722d2c6
miraj98hossain <miraj09hossain@gmail.com>
2024-12-12 20:44:35 +0600

    on boarding page ui done

--- lib/features/on_boarding/view/on_boarding_view.dart
+++ lib/features/on_boarding/view/on_boarding_view.dart
@@ -13,30 +13,100 @@ class _OnBoardingViewState extends State<OnBoardingView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).viewPadding.top,
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.88,
+            decoration: BoxDecoration(
+              color: appTheme.white,
             ),
-            Image.asset(
-              height: 100,
-              width: double.infinity,
-              ImageConstant.companyLogo,
+            child: Column(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.35 +
+                      MediaQuery.of(context).viewPadding.top,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: appTheme.skyBlue,
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).viewPadding.top,
+                      ),
+                      const SizedBox(
+                        height: 35,
+                      ),
+                      Image.asset(
+                        height: 180,
+                        width: 353,
+                        ImageConstant.directorPortal,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 45,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Delta Hospital",
+                      style: lightTextTheme.bodyLarge!.copyWith(
+                        fontWeight: FontWeight.w700,
+                        color: appTheme.primary,
+                      ),
+                    ),
+                    Text(
+                      "Please Sign In/Sign Up to Continue",
+                      style: lightTextTheme.bodySmall!.copyWith(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 45,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // context.pushNamed(DashboardVerifyOtpPage.routeName);
+                        },
+                        child: Text(
+                          "Sign In",
+                          style: lightTextTheme.bodySmall!.copyWith(
+                            fontWeight: FontWeight.w600,
+                            color: appTheme.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // context.pushNamed(DashboardVerifyOtpPage.routeName);
+                        },
+                        child: Text(
+                          "Sign Up",
+                          style: lightTextTheme.bodySmall!.copyWith(
+                            fontWeight: FontWeight.w600,
+                            color: appTheme.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Text(
-              "Welcome To",
-              style: lightTextTheme.bodyMedium!.copyWith(
-                fontWeight: FontWeight.w600,
-                fontSize: 30,
-                color: appTheme.white,
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
