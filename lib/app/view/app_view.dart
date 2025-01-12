c32d4dfb7a5ec091f5828951f3fa79d58524fce0
miraj98hossain <miraj09hossain@gmail.com>
2025-01-12 20:51:36 +0600

    feat: Add Flutter localizations for internationalization support & to fix Date picker Date format

--- lib/app/view/app_view.dart
+++ lib/app/view/app_view.dart
@@ -7,6 +7,7 @@ import 'package:delta_hospital/core/theme/app_theme.dart';
 import 'package:delta_hospital/dependency_injector/di_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class AppView extends StatelessWidget {
   const AppView({super.key});
@@ -34,6 +35,14 @@ class AppView extends StatelessWidget {
         darkTheme: AppTheme.lightTheme,
         themeMode: ThemeMode.light,
         routerConfig: AppNavigation.router,
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en', 'GB'), // English, UK
+        ],
       ),
     );
   }
