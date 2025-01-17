96946207e482c435d8e9a3d65a6a52387e968702
miraj98hossain <miraj09hossain@gmail.com>
2025-01-17 21:51:05 +0600

    feat: Add PaymodeListResponse model and API method
    This commit adds the PaymodeListResponse model and the getPayModeList API method to the AppRemoteDataSource. The PaymodeListResponse model represents the response data for retrieving the list of paymodes from the server. The getPayModeList method allows fetching the list of available paymodes. This feature enhances the functionality of the app by providing access to the available paymodes for online appointments.

--- lib/app/domain/repositories/app_repository.dart
+++ lib/app/domain/repositories/app_repository.dart
@@ -2,6 +2,7 @@ import 'package:delta_hospital/app/data/models/app_login_response.dart';
 import 'package:delta_hospital/app/data/models/auth_response.dart';
 import 'package:delta_hospital/app/data/models/his_patient_info_response.dart';
 import 'package:delta_hospital/app/data/models/patient_portal_user_list_response.dart';
+import 'package:delta_hospital/app/data/models/paymode_list_response.dart';
 import 'package:delta_hospital/app/data/models/user_details_response.dart';
 
 abstract class AppRepository {
@@ -55,4 +56,5 @@ abstract class AppRepository {
   Future<HisPatientInfo> getRegPatientInfo({
     required String mrnOrPhNo,
   });
+  Future<List<Paymode>> getPayModeList();
 }
