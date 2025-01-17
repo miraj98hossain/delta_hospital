b7177f1c5d9b50744d25a69715b4ef4949670049
miraj98hossain <miraj09hossain@gmail.com>
2025-01-17 17:24:49 +0600

    feat: Add HisPatientInfoBloc for retrieving patient information

---
+++ lib/app/bloc/his_patient_info_bloc.dart
@@ -0,0 +1,46 @@
+import 'package:delta_hospital/app/data/models/his_patient_info_response.dart';
+import 'package:delta_hospital/app/data/repositories/app_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+sealed class HisPatientInfoEvent {}
+
+final class HisPatientInfoGet extends HisPatientInfoEvent {
+  final String mrnOrPhNo;
+
+  HisPatientInfoGet({required this.mrnOrPhNo});
+}
+
+@immutable
+sealed class HisPatientInfoState {}
+
+final class HisPatientInfoInitial extends HisPatientInfoState {}
+
+final class HisPatientInfoLoading extends HisPatientInfoState {}
+
+final class HisPatientInfoSuccess extends HisPatientInfoState {
+  final HisPatientInfo patientInfo;
+  HisPatientInfoSuccess({required this.patientInfo});
+}
+
+final class HisPatientInfoError extends HisPatientInfoState {
+  final Object error;
+  HisPatientInfoError({required this.error});
+}
+
+class HisPatientInfoBloc
+    extends Bloc<HisPatientInfoEvent, HisPatientInfoState> {
+  final AppRepository _appRepository;
+  HisPatientInfoBloc(this._appRepository) : super(HisPatientInfoInitial()) {
+    on<HisPatientInfoGet>((event, emit) async {
+      emit(HisPatientInfoLoading());
+      try {
+        var response =
+            await _appRepository.getRegPatientInfo(mrnOrPhNo: event.mrnOrPhNo);
+        emit(HisPatientInfoSuccess(patientInfo: response));
+      } catch (e) {
+        emit(HisPatientInfoError(error: e));
+      }
+    });
+  }
+}
