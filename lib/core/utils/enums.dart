58cc63a599c53de140d7d04a50cafbc93955452f
miraj98hossain <miraj09hossain@gmail.com>
2025-01-17 17:04:15 +0600

    feat: Add Gender enum with toString method
    This commit adds a new `Gender` enum to the `enums.dart` file. The enum includes three values: `male`, `female`, and `other`, each with a corresponding `value` property. Additionally, a `toString` method is implemented to return the `value` of the selected gender. This enhancement improves the codebase by providing a standardized way to represent and display gender values.

---
+++ lib/core/utils/enums.dart
@@ -0,0 +1,13 @@
+enum Gender {
+  male("Male"),
+  female("Female"),
+  other("Other");
+
+  const Gender(this.value);
+
+  final String value;
+  @override
+  String toString() {
+    return value;
+  }
+}
