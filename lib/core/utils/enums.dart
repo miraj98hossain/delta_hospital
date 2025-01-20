beefd3f0c01697b7b110a5ac0b43bb6ed8a0c040
miraj98hossain <miraj09hossain@gmail.com>
2025-01-20 23:17:11 +0600

    feat: Add BloodGroup enum for representing different blood groups
    This commit adds the `BloodGroup` enum to the `enums.dart` file. The `BloodGroup` enum represents different blood groups and includes values for A+, A-, B+, B-, AB+, AB-, O+, and O-. This addition enhances the functionality of the application by providing a standardized way to represent blood groups.

--- lib/core/utils/enums.dart
+++ lib/core/utils/enums.dart
@@ -11,3 +11,23 @@ enum Gender {
     return value;
   }
 }
+
+enum BloodGroup {
+  aPositive("A+"),
+  aNegative("A-"),
+  bPositive("B+"),
+  bNegative("B-"),
+  abPositive("AB+"),
+  abNegative("AB-"),
+  oPositive("O+"),
+  oNegative("O-");
+
+  const BloodGroup(this.value);
+
+  final String value;
+
+  @override
+  String toString() {
+    return value;
+  }
+}
